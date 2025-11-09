21817bd0cc964a24298a56880296083b38a389e3
Valeriy <Valera.koz.@mail.ru>
2025-11-09 17:43:04 +0700

    work

--- good/urls.py
+++ good/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from good import views
 app_name = 'good'
 urlpatterns = [
+    path('search/', views.catalog, name= "search"),
     path('<slug:category_slug>/', views.catalog, name= "catalog"),
-     path('<slug:category_slug>/<int:page>/', views.catalog, name= "catalog"),
     path('product/<slug:product_slug>/', views.product, name= "product")
 
 ]
--- good/views.py
+++ good/views.py
@@ -3,14 +3,29 @@ from django.http import HttpResponse
 from good.models import Products
 from good.models import Category
 from django.core.paginator import Paginator
+from good.utils import q_search
 # Create your views here.
-def catalog(request,category_slug,page=1):
+def catalog(request,category_slug=None):
+    page = request.GET.get('page',1)
+    on_sale = request.GET.get('on_sale',None)
+    order_by = request.GET.get('order_by',None)
+    query = request.GET.get('q',None)
+    print(query)
+
     if category_slug == 'all':
         goods= Products.objects.all()
+    elif query:
+        goods =  q_search(query)
+    
     else:
         goods= get_list_or_404(Products.objects.filter(category__slug= category_slug))
+    if on_sale:
+        goods= goods.filter(discount__gt=0)
+    if order_by and order_by != 'default' :
+        goods= goods.order_by(order_by)
+
     paginator = Paginator(goods, 3)
-    paginat= paginator.page(page)
+    paginat= paginator.page(int(page))
     contet = {'goods':paginat ,
               'slug_url': category_slug}
 
